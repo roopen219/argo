0e95d5ab5a883b27109c66bb83f6f9f5b5f48d1d
Roopendra Talekar <roopendratalekar@gmail.com>
2016-10-02 18:24:14 +0530

    Split webpack config into multiple configs

---
+++ webpack.base.config.js
@@ -0,0 +1,63 @@
+/* global __dirname */
+
+const CopyPlugin = require('copy-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+
+const PATHS = require('./webpack.paths');
+
+module.exports = {
+    entry: PATHS.app,
+    output: {
+        path: PATHS.build,
+        filename: 'build.js'
+    },
+    resolve: {
+        root: [
+            PATHS.src
+        ],
+        fallback: [PATHS.nodeModules],
+        alias: {
+            'static': PATHS.static,
+            'components': PATHS.components,
+            'scss': PATHS.scss
+        }
+    },
+    module: {
+        loaders: [{
+            test: /\.vue$/,
+            loader: 'vue'
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel',
+            exclude: /node_modules/
+        },
+        {
+            test: /\.json$/,
+            loader: 'json'
+        }, {
+            test: /\.(sass|scss)$/,
+            loaders: ['style', 'css?minimize&autoprefixer=1', 'sass']
+        }]
+    },
+    vue: {
+        loaders: {
+            sass: 'style!css!sass?indentedSyntax',
+            scss: 'style!css!sass',
+            css: 'style!css!sass'
+        }
+    },
+    sassLoader: {
+        includePaths: [PATHS.scss]
+    },
+    plugins: [
+        new CleanPlugin(['build/*.hot-update.*']),
+        new CopyPlugin([
+            {
+                from: 'electron'
+            }
+        ], {
+            copyUnmodified: true
+        })
+    ]
+};
