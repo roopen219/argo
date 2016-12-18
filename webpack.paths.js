07a495cfbc38795c44e0a03a53ce0e47a8379070
Roopendra Talekar <roopendratalekar@gmail.com>
2016-12-18 20:46:30 +0530

    fix webpack paths

--- webpack.paths.js
+++ webpack.paths.js
@@ -1,14 +1,14 @@
 const path = require('path');
 
 module.exports = {
-    app: path.join(__dirname, 'src/app.js'),
+    app: path.join(__dirname, 'client/app.js'),
     build: path.join(__dirname, 'build/'),
-    src: path.join(__dirname, 'src/'),
-    scss: path.join(__dirname, 'src/static/scss/'),
-    static: path.join(__dirname, 'src/static/'),
-    utils: path.join(__dirname, 'src/utils/'),
-    components: path.join(__dirname, 'src/components/'),
-    services: path.join(__dirname, 'src/services'),
+    src: path.join(__dirname, 'client/'),
+    scss: path.join(__dirname, 'client/static/scss/'),
+    static: path.join(__dirname, 'client/static/'),
+    utils: path.join(__dirname, 'client/utils/'),
+    components: path.join(__dirname, 'client/components/'),
+    services: path.join(__dirname, 'client/services'),
     nodeModules: path.join(__dirname, 'node_modules/'),
-    electronMain: path.join(__dirname, 'src/electron.js')
+    electronMain: path.join(__dirname, 'client/electron.js')
 };
