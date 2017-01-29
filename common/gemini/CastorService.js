593cbcb2d222f9d8730d34d0834608a3143cc925
Roopendra Talekar <roopendratalekar@gmail.com>
2017-01-29 09:03:05 +0530

    code style fix

--- common/gemini/CastorService.js
+++ common/gemini/CastorService.js
@@ -7,24 +7,33 @@ import METHODS from './geminiMethods'
 let methods = Object.keys(METHODS).map((methodKey) => METHODS[methodKey].name)
 
 class CastorService extends EventEmitter {
+
     constructor(serviceName, options) {
+
         if (!serviceName) {
             throw new Error('Missing service name')
         }
+
         super()
+
         let nonCustomProperties = methods.concat(['hooks'])
+
         this._methods = _.pick(options, methods)
         this._hooks = _.pick(options.hooks, ['before', 'after'])
         this._setup = options.setup
         this.name = serviceName
 
         Object.keys(options).forEach((option) => {
+
             if (nonCustomProperties.indexOf(option) === -1) {
+
                 if (_.isFunction(options[option])) {
                     this[option] = options[option].bind(this)
                 }
                 this[option] = options[option]
+
             }
+
         })
 
         Object.keys(this._methods).forEach((method) => {
@@ -34,21 +43,29 @@ class CastorService extends EventEmitter {
         methods.forEach((method) => {
             this[method] = this._wrapMethod(method)
         })
+
     }
 
     setup(geminiApp) {
+
         if (_.isFunction(this._setup)) {
             this._setup(geminiApp)
         }
 
         this.app = geminiApp
+
     }
 
     _wrapMethod(method) {
+
         return (params, client) => {
+
             if (!this._methods[method]) {
+
                 return Promise.reject(new Error(`${method} not implemented on this service`))
+
             } else {
+
                 client = client || {}
                 params = params || {}
 
@@ -74,26 +91,37 @@ class CastorService extends EventEmitter {
 
                         return result
                     })
+
             }
+
         }
+
     }
 
     _executeHooks(type, method, hookObject) {
+
         let isHookPresent = this._hooks[type] && this._hooks[type][method]
 
         if (isHookPresent) {
+
             let hooks = this._hooks[type][method]
 
             if (_.isArray(hooks)) {
+
                 hooks.reduce((promise, hook) => promise.then(() => hook.call(this, hookObject)), Promise.resolve())
+
             } else {
+
                 hooks.call(this, hookObject)
+
             }
 
         }
 
         return Promise.resolve(type === 'before' ? hookObject.params : hookObject.result)
+
     }
+
 }
 
 export default CastorService
