5e4178a200b199e40ea07d745dfb3508932f3277
Roopendra <roopendratalekar@gmail.com>
2017-02-12 21:12:40 +0530

    added socket io and castor to pollux tests code

--- common/gemini/specs/Pollux.spec.js
+++ common/gemini/specs/Pollux.spec.js
@@ -1,10 +1,43 @@
 /*global Promise*/
 
 import test from 'ava'
+import io from 'socket.io'
+import ioClient from 'socket.io-client'
+
 import Pollux from '../Pollux'
+import Castor from '../Castor'
 
 test.beforeEach(t => {
     t.context.pollux = new Pollux()
+    t.context.socketIOServer = io()
+    t.context.castor = new Castor(t.context.socketIOServer)
+    t.context.port = Math.floor(3000 + (Math.random() * 10000) % 10000)
+    t.context.socketIOServer.listen(t.context.port)
+    t.context.socketIOClient = ioClient('http://localhost:' + t.context.port)
+
+    t.context.loginData = {
+        name: 'JohnDoe',
+        password: 'password123'
+    }
+
+    t.context.castor.use('user', {
+        data: {
+            _id: 1,
+            ...t.context.loginData
+        },
+        find(params) {
+            if (params.name === this.data.name) {
+                return Promise.resolve(this.data)
+            }
+            return Promise.reject('No such user exists!')
+        },
+        comparePassword(password) {
+            if (password === this.data.password) {
+                return Promise.resolve(true)
+            }
+            return Promise.reject('Password doesn\'t match!')
+        }
+    })
 })
 
 test('register a service and call a method', t => {
