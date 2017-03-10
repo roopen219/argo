1b15e57fe4fe97d9a04a6b084840e3288eb8a0a3
Roopendra Talekar <roopendratalekar@gmail.com>
2017-03-10 19:21:18 +0530

    added (ADD|REMOVE)_TAB_GROUP to types

--- client/store/types.js
+++ client/store/types.js
@@ -2,6 +2,8 @@
 export const ADD_TAB = 'tabs/ADD_TAB'
 export const REMOVE_TAB = 'tabs/REMOVE_TAB'
 export const SWITCH_TAB = 'tabs/SWITCH_TAB'
+export const ADD_TAB_GROUP = 'tabs/ADD_TAB_GROUP'
+export const REMOVE_TAB_GROUP = 'tabs/REMOVE_TAB_GROUP'
 
 // PROTOTYPE
 export const CREATE_PROTOTYPE = 'prototype/CREATE_PROTOTYPE'
