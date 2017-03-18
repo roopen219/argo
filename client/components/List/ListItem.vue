b5d0e1ccea1a43e04355ec4b4c7f301eaa148871
Roopendra Talekar <roopendratalekar@gmail.com>
2017-03-18 11:17:37 +0530

    added ListView component

---
+++ client/components/List/ListItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <li v-if="listItem">{{this.listItem.name}} &mdash; {{Date(this.listItem.meta.created)}}</li>
+</template>
+
+<script>
+    export default {
+        name: 'argo-list-item',
+        props: ['listItem'],
+        created: function() {
+            console.log(this.listItem)
+        }
+    }
+</script>
---
+++ client/components/List/ListView.vue
@@ -0,0 +1,31 @@
+<style>
+</style>
+
+<template>
+    <ul class="flex-column">
+        <component  :is="listItemComponent"
+                    v-for="(listItem, index) in listItems"
+                    :index="index"
+                    :listItem="listItem">
+        </component>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'argo-list-view',
+        props: {
+            listItems: {
+                type: Array,
+                default: []
+            },
+            listItemComponent: {
+                type: String,
+                default: 'argo-list-item'
+            }
+        },
+        created: function (){
+            console.log(this.listItems)
+        }
+    }
+</script>
