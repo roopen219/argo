25c4c9d1b6c34d84f9102e3f0ae06cc14f9a5e6e
Roopendra Talekar <roopendratalekar@gmail.com>
2017-03-21 23:58:19 +0530

    started working on location bar for new tab

--- client/components/NewTab.vue
+++ client/components/NewTab.vue
@@ -1,6 +1,9 @@
 <style lang="scss">
+    @import "variables";
+
     .new-tab-view {
         padding: 20px;
+        background: #fff;
 
         .list {
             padding: 0;
@@ -8,13 +11,30 @@
             .list-item {
                 list-style: none;
             }
+        }
 
+        .location-bar {
+            padding: 4px 20px;
+            margin: -20px -20px 20px -20px;
+            background: $bg-tab-active;
+            border-bottom: 1px solid $color-tabs-border;
+
+            input[type="text"] {
+                margin-left: 12px;
+            }
         }
     }
 </style>
 
 <template>
     <div class="flex-grow-1 new-tab-view">
+        <div class="location-bar flex-row">
+            <div class="btn-group">
+                <button class="prev btn-default">&laquo;</button>
+                <button class="next btn-default">&raquo;</button>
+            </div>
+            <input type="text" class="input-text flex-grow-1" placeholder="Open...">
+        </div>
         <button class="btn-primary" @click="createPrototype">+ Create New</button>
         <argo-list  :listItems="listOfPrototypes"
                     class="list"
