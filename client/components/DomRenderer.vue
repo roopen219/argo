7ffa760deb29b50569c352def352728a3476a0fa
Roopendra Talekar <roopendratalekar@gmail.com>
2017-03-28 23:24:06 +0530

    added the dom rendering logic for prototype

---
+++ client/components/DomRenderer.vue
@@ -0,0 +1,19 @@
+<style></style>
+
+<script>
+    export default {
+        name: 'argo-dom-renderer',
+        render: function (createElement) {
+            return createElement('div', renderChildren(createElement, this.dom.root.children))
+        },
+        props: ['dom']
+    }
+
+    function renderChildren (createElement, children = []) {
+        return children.map(function(child) {
+            return createElement(child.tagName, {
+                'class': child.class
+            }, renderChildren(createElement, child.children))
+        })
+    }
+</script>
---
+++ client/components/PrototypeEditor.vue
@@ -0,0 +1,57 @@
+<style lang="scss">
+    .location-bar {
+        .prev,
+        .next {
+            padding: 4px 8px;
+        }
+
+        .btn-default {
+            font-size: 12px;
+        }
+    }
+
+    .editor {
+        overflow: hidden;
+    }
+
+    .canvas-wrapper {
+        background: #fff;
+        overflow: hidden;
+    }
+
+    .canvas {
+        background: aqua;
+        overflow: auto;
+    }
+
+    .property-panel {
+        background: aquamarine;
+    }
+</style>
+
+<template>
+    <div class="editor flex-column flex-grow-1">
+        <argo-location-bar>
+            <div class="btn-group flex-row">
+               <button class="prev btn-default"><i class="mdi-arrow-left"></i></button>
+               <button class="next btn-default"><i class="mdi-arrow-right"></i></button>
+            </div>
+        </argo-location-bar>
+
+        <div class="flex-grow-1 canvas-wrapper flex-row">
+            <div class="canvas flex-grow-9">
+                <argo-dom-renderer :dom="tabContent.dom"></argo-dom-renderer>
+            </div>
+            <div class="property-panel flex-grow-1"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapActions} from 'vuex'
+
+    export default {
+        name: 'argo-prototype-editor',
+        props: ['tabContent', 'tabIndex']
+    }
+</script>
