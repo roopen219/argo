69ac7354b4d29e5de0537f8901dc9be2da48d93e
Roopendra Talekar <roopendratalekar@gmail.com>
2017-03-29 23:46:47 +0530

    bunch of rendering logic refactor in DomRenderer

--- client/components/DomRenderer.vue
+++ client/components/DomRenderer.vue
@@ -4,16 +4,32 @@
     export default {
         name: 'argo-dom-renderer',
         render: function (createElement) {
-            return createElement('div', renderChildren(createElement, this.dom.root.children))
+            return createElement('div', renderChildren(createElement, this.sharedStyles, this.dom.root.children))
         },
-        props: ['dom']
+        props: ['dom', 'sharedStyles']
     }
 
-    function renderChildren (createElement, children = []) {
+    function renderChildren (createElement, sharedStyles, children = []) {
+
         return children.map(function(child) {
             return createElement(child.tagName, {
-                'class': child.class
-            }, renderChildren(createElement, child.children))
+                props: {
+                    ...child.props,
+                    sharedStyles
+                },
+                attrs: child.attrs,
+                style: [child.style, computeStyle(sharedStyles, child.class)]
+            }, child.textContent || renderChildren(createElement, sharedStyles, child.children))
+
         })
+
+    }
+
+    function computeStyle (sharedStyles= {}, classes = []) {
+
+        return classes.map(function (_class) {
+            return sharedStyles[_class]
+        })
+
     }
 </script>
